cfac2ce17e5405d31f793da814a16b4baff412c2
samson tedla <samitedla23@gmail.com>
2021-12-22 20:53:27 +0000

    DBStorage - States and Cities

--- models/base_model.py
+++ models/base_model.py
@@ -1,31 +1,45 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-import uuid
+from uuid import uuid4
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
-from sqlalchemy import Column, String, distinct, DATETIME
-"""
+from sqlalchemy import Column, String, distinct, DateTime
+from models import storage_type
+
+
 Base = declarative_base
-"""
 
 class BaseModel:
     """A base class for all hbnb models"""
-    """id = Column(String(60),
+    
+    id = Column(String(60),
                 nullable=False,
                         primary_key=True).distinct()
     created_at = Column(nullable=False,
                         default=datetime.utcnow())
     updated_at = Column(nullable=False,
                         default=datetime.utcnow())
-    """
+    
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+        self.id = str(uuid4())
+        self.created_at = self.updated_at = datetime.utcnow()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+
+        """
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+             
+            #storage.new(self)
+            
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -33,7 +47,7 @@ class BaseModel:
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
             self.__dict__.update(kwargs)
-
+        """
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
@@ -43,14 +57,28 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
+
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = str(type(self).__name__)
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        my_dict.pop("_sa_instance_state", None)
+        """
         dictionary = {}
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        """
+        return my_dict
+
+    def delete(self):
+        """delete the current instance from storage"""
+        from models import storage
+        storage.delete(self)
--- models/place.py
+++ models/place.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.review import Review
+from models.base_model import BaseModel, Base
+from models import storage_type
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.sql.schema import Table
+from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel):
