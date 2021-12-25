d712f81aeb2029926be06254855ac07f40e60aa3
samson tedla <samitedla23@gmail.com>
2021-12-25 19:56:46 +0000

    edited city

--- models/city.py
+++ models/city.py
@@ -13,7 +13,15 @@ from sqlalchemy.orm import relationship
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    name = Column(String(128), nullable=False)
-    __tablename__ = "cities"
-    places = relationship("Place", backref="cities", cascade="delete")
+    if models.storage_t == "db":
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        name = Column(String(128), nullable=False)
+        __tablename__ = "cities"
+        places = relationship("Place", backref="cities", cascade="delete")
+    else:
+        state_id = ""
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
