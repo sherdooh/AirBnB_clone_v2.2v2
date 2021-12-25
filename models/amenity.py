ed279778e8e0edee3068971493e18dde27bf5d94
samson tedla <samitedla23@gmail.com>
2021-12-25 21:10:33 +0000

    merge conflicts

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-<<<<<<< HEAD
 from os import getenv
 import sqlalchemy
 import models
@@ -8,11 +7,6 @@ from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy import Column
 from sqlalchemy import String
-=======
-from models.base_model import Base, BaseModel
-from models import storage_type
-from sqlalchemy import Column, String
->>>>>>> 96d932ff964b5c41b97f579bcd2e1b508fbbb4fe
 from sqlalchemy.orm import relationship
 
 
@@ -20,7 +14,6 @@ class Amenity(BaseModel, Base):
     """
     Represents an Amenity for a MySQL database
     """
-<<<<<<< HEAD
     if models.storage_t == 'db':
         __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
@@ -30,10 +23,3 @@ class Amenity(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes Amenity"""
         super().__init__(*args, **kwargs)
-=======
-    __tablename__ = "amenities"
-    if storage_type == 'db':
-        name = Column(String(128), nullable=False)
-    else:
-        name = ''
->>>>>>> 96d932ff964b5c41b97f579bcd2e1b508fbbb4fe
--- models/base_model.py
+++ models/base_model.py
@@ -31,7 +31,6 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != "__class__":
                     setattr(self, key, value)
-<<<<<<< HEAD
             if kwargs.get("created_at", None) and type(self.created_at) is str:
                 self.created_at = datetime.strptime(kwargs["created_at"], time)
             else:
@@ -46,15 +45,6 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = self.created_at
-=======
-            if storage_type == 'db':
-                if not hasattr(kwargs, 'id'):
-                    setattr(self, 'id', str(uuid.uuid4()))
-                if not hasattr(kwargs, 'created_at'):
-                    setattr(self, 'created_at', datetime.now())
-                if not hasattr(kwargs, 'updated_at'):
-                    setattr(self, 'updated_at', datetime.now())
->>>>>>> 96d932ff964b5c41b97f579bcd2e1b508fbbb4fe
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -71,7 +61,6 @@ class BaseModel:
         """Convert instance into dict format"""
 
         my_dict = self.__dict__.copy()
-<<<<<<< HEAD
         if "created_at" in new_dict:
             my_dict["created_at"] = my_dict["created_at"].strftime(time)
         if "updated_at" in new_dict:
@@ -79,14 +68,6 @@ class BaseModel:
         my_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in my_dict:
             del my_dict["_sa_instance_state"]
-=======
-        my_dict["__class__"] = str(type(self).__name__)
-        my_dict["created_at"] = self.created_at.isoformat()
-        my_dict["updated_at"] = self.updated_at.isoformat()
-        if hasattr(my_dict, "_sa_instance_state"):
-            my_dict.pop("_sa_instance_state", None)
-
->>>>>>> 96d932ff964b5c41b97f579bcd2e1b508fbbb4fe
         return my_dict
 
     def delete(self):
